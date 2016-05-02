9855aaa1f081cc37a5212e3a55d96be78ca6dcdf
Tobias Eriksson <tobias@wedogames.se>
2016-05-02 21:57:40 +0200

    Adjust window size based on current platform for easier development
    This is for making it easier to switch between developing on my laptop and my desktop as long as these values are hard coded.

--- main.go
+++ main.go
@@ -74,8 +74,16 @@ func main() {
 	img.Init(img.INIT_PNG)
 	ttf.Init()
 
+	var windowFlags uint32 = sdl.WINDOW_SHOWN
+
+	if runtime.GOOS == `darwin` {
+		ScreenWidth = 1280
+		ScreenHeight = 800
+		windowFlags = sdl.WINDOW_FULLSCREEN
+	}
+
 	window, err := sdl.CreateWindow("Astrotyper", sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED, int(ScreenWidth), int(ScreenHeight), sdl.WINDOW_SHOWN)
+		sdl.WINDOWPOS_UNDEFINED, int(ScreenWidth), int(ScreenHeight), windowFlags)
 	if err != nil {
 		panic(err)
 	}
